b2c2c14d9d505a9e01017b4e6cf31eea9c5c1176
Nurlan <nurlanea@code.edu.az>
2023-11-06 23:10:47 +0400

    upload

---
+++ Reverse/Models/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse.Models
+{
+    class Account
+    {
+        public string Username;
+        public string Password;
+
+        public string Login (string username, string password)
+        {
+            if ((Username == username) && (Password == password))
+            {
+                return "Login successful";
+            };
+            return $"Username or password is incorrect";
+        }
+
+        public Account(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
+//public string Login(string username, string password)
+//{
+//    if ((Username == username) && (Password ==password))
+//    {
+//        return "Login successful";
+//    };
+//    return "Username or password is incorrect";
+//}
---
+++ Reverse/Program.cs
@@ -0,0 +1,44 @@
+﻿#region Reverse 
+//using System.Text;
+
+//Console.WriteLine(Reverse("Salam necesen"));
+//string Reverse (string text)
+//{
+//    StringBuilder builder = new StringBuilder();
+//    for (int i = text.Length - 1; i >= 0; i--)
+//    {
+//        builder.Append(text[i]);
+//    }
+//    return builder.ToString();
+//}
+#endregion
+
+#region login
+//using Reverse.Models;
+//Console.WriteLine("Write your username");
+//string user = Console.ReadLine();
+//Console.WriteLine("Write your password");
+//string pass = Console.ReadLine();
+
+//Account account1 = new(username: "lorem123", password: "123456*");
+//Console.WriteLine(account1.Login(user,pass));
+#endregion
+
+#region factorial
+//using Reverse.Models;
+//using System.Threading.Channels;
+
+//Factorial factorial1 = new(3);
+//Factorial factorial2 = new(10);
+//Console.WriteLine(factorial1.Function(3));
+//Console.WriteLine(factorial2.Function(10));
+#endregion
+
+using Reverse.Models;
+
+int Value1= int.Parse(Console.ReadLine());
+char Symbol = char.Parse(Console.ReadLine());
+int Value2= int.Parse(Console.ReadLine());
+
+Calculator calculation = new Calculator(Value1,Symbol,Value2);
+Console.WriteLine(calculation.Calculate());
